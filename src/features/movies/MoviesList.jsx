a9b4f065eeee77f783fd0dbca60d529d4ac49469
Igor Baranov <baranow.ig@gmail.com>
2024-10-09 16:51:44 +0200

    Initial setup Layout with ChakraUI of MoviesList, MovieCard and MovieDetails page.

--- src/features/movies/MoviesList.jsx
+++ src/features/movies/MoviesList.jsx
@@ -1,11 +1,26 @@
-import { Link } from "react-router-dom";
+import { Heading, SimpleGrid } from "@chakra-ui/react";
+import { data } from "./data";
+import MovieCard from "./MovieCard";
+import { MOVIEDB_IMAGES_URL } from "src/common/constants";
 
 function MoviesList() {
   return (
-    <div>
-      MoviesList
-      <Link to={"/movies/dsds"}>Go to movie</Link>
-    </div>
+    <>
+      <Heading textAlign="center" size="xl" mb={6}>
+        Trending Movies
+      </Heading>
+      <SimpleGrid spacing={4} columns={{ sm: 1, md: 3, lg: 4 }}>
+        {data.results.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            poster={`${MOVIEDB_IMAGES_URL}/${movie.poster_path}`}
+            overview={movie.overview}
+          />
+        ))}
+      </SimpleGrid>
+    </>
   );
 }
 
