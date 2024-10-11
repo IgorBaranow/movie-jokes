91b00f09d0a61b4305db8481e3d2b2c1f0e64d7d
Igor Baranov <baranow.ig@gmail.com>
2024-10-11 20:13:54 +0200

    Set up Redux and move all the data to the store

--- src/features/movies/MoviesList.jsx
+++ src/features/movies/MoviesList.jsx
@@ -1,16 +1,18 @@
 import { Heading, SimpleGrid } from "@chakra-ui/react";
-import { data } from "./data";
 import MovieCard from "./MovieCard";
 import { MOVIEDB_IMAGES_URL } from "src/common/constants";
+import { useSelector } from "react-redux";
+import { selectAllMovies } from "./moviesSlice";
 
 function MoviesList() {
+  const movies = useSelector(selectAllMovies);
   return (
     <>
       <Heading textAlign="center" size="xl" mb={6}>
         Trending Movies
       </Heading>
       <SimpleGrid spacing={4} columns={{ sm: 1, md: 3, lg: 4 }}>
-        {data.results.map((movie) => (
+        {movies.map((movie) => (
           <MovieCard
             key={movie.id}
             id={movie.id}
